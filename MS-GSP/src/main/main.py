5d037c83bef16093ed32996669d28fd68601caee
Gary Turovsky <flyhighplato@gmail.com>
2011-01-30 15:54:19 -0600

    Added loadParam function for loading the parameter file

--- MS-GSP/src/main/main.py
+++ MS-GSP/src/main/main.py
@@ -12,18 +12,25 @@ import re
 # The sequence database
 seqDB = []
 
+# The parameters
+misMap = {}
+
 # The path to the input data
 dataPath = "../../../Data/data.txt"
+
+# The path to the MIS parameter data
+paramPath = "../../../Data/para.txt"
+
+# Support difference constraint
+sdc = 0.0
 
 #### I/O
 
 def loadData( db, fileName ):
-    print( "opening: ", fileName )
     FILE = open( fileName, "r" )
     for line in FILE:
         line = line.rstrip('\n')
         seqStr = re.split( r"<{|}{|}>", line )
-        print( line )
         seqLst = []
         for transStr in seqStr:
             if ( transStr != '' ):
@@ -33,8 +40,27 @@ def loadData( db, fileName ):
                     transLst.append( int(transItem) )
                 seqLst.append( transLst )
         db.append( seqLst )
+
+def loadParam( map, fileName):
+    FILE = open( fileName, "r" )
+    for line in FILE:
+        line = line.rstrip('\n')
+        param = re.findall(r"\d+\.*\d*",line)
+        
+        #Make sure we're dealing with the two types of parameters that we can handle
+        assert( ( len( param ) == 2 and line.startswith("MIS") ) or ( len( param ) == 1 and line.startswith( "SDC" ) ) )
+        
+        #Specifying minimum support for an item
+        if(line.startswith("MIS")):
+            map[int(param[0])] = float(param[1])
+        #Specifying support difference constraint
+        else:
+            sdc = float(param[0])
         
 if __name__ == '__main__':
     loadData( seqDB, dataPath )
-    print ( "-------------")
-    print ( seqDB )
+    loadParam(misMap,paramPath)
+    
+    print( seqDB )
+    print( misMap ) 
+    print( sdc )
