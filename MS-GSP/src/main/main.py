3f5f6e628bf2d5e382ba6717f461703cc82b22f5
Gary Turovsky <flyhighplato@gmail.com>
2011-01-30 13:49:33 -0600

    Removing data folder. No longer tracking it in Eclipse (not sure how to get both of our Eclipses to see that folder).

--- MS-GSP/src/main/main.py
+++ MS-GSP/src/main/main.py
@@ -5,5 +5,36 @@ Created on Jan 26, 2011
 @author: alanperezrathke
 '''
 
+import re
+
+#### Global variables
+
+# The sequence database
+seqDB = []
+
+# The path to the input data
+dataPath = "../../../Data/data.txt"
+
+#### I/O
+
+def loadData( db, fileName ):
+    print( "opening: ", fileName )
+    FILE = open( fileName, "r" )
+    for line in FILE:
+        line = line.rstrip('\n')
+        seqStr = re.split( r"<{|}{|}>", line )
+        print( line )
+        seqLst = []
+        for transStr in seqStr:
+            if ( transStr != '' ):
+                transLst = []
+                transItemsLst = transStr.split( ", " ) 
+                for transItem in transItemsLst:
+                    transLst.append( int(transItem) )
+                seqLst.append( transLst )
+        db.append( seqLst )
+        
 if __name__ == '__main__':
-    print('Hello MS-GSP!')
+    loadData( seqDB, dataPath )
+    print ( "-------------")
+    print ( seqDB )
